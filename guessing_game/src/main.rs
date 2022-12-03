56a01e2fe617024961c8d3a93d3dd322fc56cabf
Matthew Stagg <matthewmstagg@gmail.com>
2022-12-02 22:00:10 -0500

    finishing guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let target = rand::thread_rng().gen_range(1..=1000);
+
+    println!("Welcome! Today we will guess a number...");
+
+    loop {
+        println!("Input your guess: ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(x) => x,
+            Err(_) => {
+                println!("{} is not a number. You idiot!", guess.trim());
+                continue;
+            }
+        };
+
+        match guess.cmp(&target) {
+            Ordering::Less => println!("{guess} is too small. Think bigger!"),
+            Ordering::Greater => println!("{guess} is too big. Think smaller!"),
+            Ordering::Equal => {
+                println!("{guess} is correct. You win!");
+                break;
+            }
+        }
+    }
 }
