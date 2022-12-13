dbefb3423a83e091f72d6bb79787e74c7a07b5b4
Matthew Stagg <matthewmstagg@gmail.com>
2022-12-13 16:55:07 -0500

    learned about clippy

--- ch8_exercises/src/median_and_mode.rs
+++ ch8_exercises/src/median_and_mode.rs
@@ -9,41 +9,42 @@ pub fn median(v: &Vec<i32>) -> Option<f64> {
     let v_size = v.len() as i32;
     v_clone.sort();
 
-    if v_size == 0 {
-        return None;
+    if v.is_empty() {
+        None
     } else if v_size == 1 {
-        return Some(v_clone[0] as f64);
+        Some(v_clone[0] as f64)
     } else if v_size % 2 != 0 {
         let median_index = div_ceil(&v_size, &2);
-        return Some(v_clone[(median_index - 1) as usize] as f64);
+        Some(v_clone[(median_index - 1) as usize] as f64)
     } else {
         let median_index_high = div_ceil(&v_size, &2);
         let median_index_low = (v_size / 2) - 1;
         let median_sum = v_clone[median_index_high as usize] + v_clone[median_index_low as usize];
-        return Some(median_sum as f64 / 2.0);
+        Some(median_sum as f64 / 2.0)
     }
 }
 
 pub fn mode(v: &Vec<i32>) -> Option<Vec<i32>> {
-    if v.len() == 0 {
+    if v.is_empty() {
         return None;
     }
-    let mut v_mode = Vec::new();
+
     let mut number_count_map = HashMap::new();
     for x in v {
         let count = number_count_map.entry(*x).or_insert(0);
         *count += 1;
     }
 
-    let mut mode_count = 0;
-    for (_, val) in &number_count_map {
-        if *val > mode_count {
-            mode_count = *val;
+    let mut highest_occurance = 0;
+    for val in number_count_map.values() {
+        if *val > highest_occurance {
+            highest_occurance = *val;
         }
     }
 
+    let mut v_mode = Vec::new();
     for (key, val) in &number_count_map {
-        if *val == mode_count {
+        if *val == highest_occurance {
             v_mode.push(*key);
         }
     }
