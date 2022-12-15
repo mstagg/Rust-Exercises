e75cd64c1651c54ebc0a6c0826ba557e644d3ae2
Matthew Stagg <matthewmstagg@gmail.com>
2022-12-14 22:32:39 -0500

    clippy fixes

--- ch8_exercises/src/pig_latin.rs
+++ ch8_exercises/src/pig_latin.rs
@@ -2,7 +2,7 @@ const VOWELS: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
 
 pub fn convert(s: &str) -> String {
     s.split_whitespace()
-        .map(|w| translate(w))
+        .map(translate)
         .collect::<Vec<String>>()
         .join(" ")
 }
@@ -12,16 +12,16 @@ fn translate(s: &str) -> String {
     let s_only_letters = only_letters(&s_copy);
 
     let mut translated_word = String::new();
-    let mut chars = s_only_letters.chars().enumerate();
+    let chars = s_only_letters.chars().enumerate();
 
-    while let Some((idx, c)) = chars.next() {
+    for (idx, c) in chars {
         if idx == 0 && is_vowel(&c) {
             translated_word.push_str(&s_only_letters);
             translated_word.push_str("-hay");
             break;
         } else if !is_vowel(&c) {
             translated_word.push_str(&s_only_letters[idx + 1..]);
-            translated_word.push_str("-");
+            translated_word.push('-');
             translated_word.push(c.to_ascii_lowercase());
             translated_word.push_str("ay");
             break;
